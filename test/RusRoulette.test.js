d3f0de9fd4f6873879d7babd247b759d70b8e6a5
enzo jakobasch <enzo.jakobasch@gmail.com>
2023-02-08 15:31:08 +0100

    push

--- test/RusRoulette.test.js
+++ test/RusRoulette.test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { assert } = require("chai");
 const { ethers } = require("hardhat");
 const Helper = require("./shared/setup");
 
@@ -12,8 +13,21 @@ describe("RightBullet", function () {
     contract = await Helper.setupContract([user1.address, user2.address]);
   });
 
-  it("Owner is msg.sender ? (should be)", async function () {
-    const ownerAddress = await contract.owner();
-    expect(ownerAddress).to.equal(owner.address);
+  it("Should initialize the contract correctly ? (should be)", async function () {
+    assert.equal(
+      await contract.owner(),
+      owner.address,
+      "Owner is not set correctly"
+    );
+    assert.equal(
+      await contract.balanceOfContract(),
+      1,
+      "Balance is not 1 ether"
+    );
+    assert.equal(
+      await contract.balanceOfJackpot(),
+      0.06,
+      "Jackpot balance is not 0.06 ether"
+    );
   });
 });
